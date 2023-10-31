8f378800e58e1b3efc626761aaac491811387e6f
Andrew <kyzwork@yandex.ru>
2023-10-31 08:28:35 +0500

    simply, but not correct

--- mongodata.py
+++ mongodata.py
@@ -9,13 +9,22 @@ dt_from = datetime.datetime(2022, 2, 1, 0, 0)
 dt_upto = datetime.datetime(2022, 2, 2, 0, 0)
 group_type = "hour"
 
-data_range ={"$and": [{"dt": {"$gt": dt_from}}, {"dt": {"$lt": dt_upto}}]}
+data_range = {"$and": [{"dt": {"$gt": dt_from}}, {"dt": {"$lt": dt_upto}}]}
 
 result = collection.find(data_range).sort("dt", 1)
 
 summary = 0
 
-for value in result:
-    summary += value['value']
+labels = [dt_from.isoformat()]
+dataset = []
 
-print(summary)
+for value in result:
+    if datetime.datetime.fromisoformat(labels[-1]).__getattribute__(group_type) == value['dt'].__getattribute__(group_type):
+        summary += value['value']
+    else:
+        labels.append(value['dt'].isoformat())
+        dataset.append(summary)
+        summary = value['value']
+
+print(dataset)
+print(labels)
